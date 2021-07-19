c39b63ca2e51b7b9fdd673b44a8e1db6f539ff9e
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-07-19 20:59:59 +0300

    Try I

--- Basketball.js
+++ Basketball.js
@@ -1,4 +1,27 @@
 class Basketball {
+    constructor(leagueName, maxPlayers) {
+        this.leagueName = leagueName;
+        this.maxPlayers = maxPlayers;
+        this.team = [];
+        this.playersList = [];
+    }
+
+    wannaBePlayer(playerName, playerPrice) {
+        let playerObject = { playerName, playerPrice }
+        this.playersList.push(playerObject);
+        console.log(playerObject);
+    }
+
+    createTeam(teamName) {
+        this.team.push({ teamName });
+        console.log(`A ${teamName} just entered a game!`);
+    }
+
+    buyPlayer(teamId, playerId) {
+
+
+    }
+
 
 }
 
